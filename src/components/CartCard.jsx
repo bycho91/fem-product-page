7f639b65e1a660c7773dc1dde635c798b0c2a742
bycho91 <bycho1991@gmail.com>
2022-04-26 18:15:46 -0500

    working on styling

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Divider,
   Heading,
   HStack,
@@ -15,21 +16,42 @@ const CartCard = () => {
   return (
     <Box borderRadius="lg" w="300px" shadow="lg">
       <Text fontSize="xl" fontWeight="bold" p=".5em">
-        Cart
+        Your Cart
       </Text>
       <Divider />
-      <HStack p="1em" justifyContent="space-evenly">
-        <Image src={ProudctImg} w="40px" h="40px" borderRadius="lg" />
+      <HStack p=".7em" justifyContent="space-evenly">
+        <Image src={ProudctImg} w="50px" h="50px" borderRadius="lg" />
         <Box>
           <Text fontSize="sm" isTruncated>
             Autum Limited Edition
           </Text>
-          <Text fontSize="sm">$125.00 x 3</Text>
+          <Text fontSize="sm" opacity="70%">
+            $125.00 x 3
+            <span
+              style={{
+                fontWeight: 'bold',
+                marginLeft: '.6em',
+                opacity: '100%',
+              }}
+            >
+              $375.00
+            </span>
+          </Text>
         </Box>
-        <Icon w="20px" h="20px">
+        <Icon
+          w="20px"
+          h="20px"
+          opacity="60%"
+          _hover={{ cursor: 'pointer', transform: 'scale(1.1)' }}
+        >
           <DeleteIcon />
         </Icon>
       </HStack>
+      <Box p=".5em">
+        <Button colorScheme="orange" width="100%">
+          Checkout
+        </Button>
+      </Box>
     </Box>
   );
 };
